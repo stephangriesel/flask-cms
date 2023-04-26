a8fedbb7f55f5a21833ec40b8297165289db5c67
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-04-26 13:09:02 +0200

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   mode: 'jit',
-  content: ["./templates/**/*.{html,htm}"],
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
