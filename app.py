ea826bc72775a806da8451c19085920f3db687a0
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-04-26 16:37:31 +0200

    static routes

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
 from content.blueprint import content
+from content.blueprint import static
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 app.register_blueprint(content, url_prefix='/content')
 # localhost:5050/content
+
+app.register_blueprint(static, url_prefix='/static')
+# localhost:5050/static
--- content/blueprint.py
+++ content/blueprint.py
@@ -3,27 +3,39 @@ from flask import render_template
 
 content = Blueprint('content', __name__, template_folder='templates')
 
-# localhost:5050/mediapack
+# localhost:5050/content/mediapack
 @content.route('/mediapack')
 def content_list_mediapack():
     return render_template('content/mediapack.html')
 
-# localhost:5050/forms
+# localhost:5050/content/forms
 @content.route('/forms')
 def content_list_forms():
     return render_template('content/forms.html')
 
-# localhost:5050/projectvideos
+# localhost:5050/content/projectvideos
 @content.route('/projectvideos')
 def content_list_projectvideos():
     return render_template('content/projectvideos.html')
 
-# localhost:5050/annualreports
+# localhost:5050/content/annualreports
 @content.route('/annualreports')
 def content_list_annualreports():
     return render_template('content/annualreports.html')
 
-# localhost:5050/articlelinks
+# localhost:5050/content/articlelinks
 @content.route('/articlelinks')
 def content_list_articlelinks():
     return render_template('content/articlelinks.html')
+
+static = Blueprint('static', __name__, template_folder='templates')
+
+# localhost:5050/static/donations
+@static.route('/donations')
+def static_list_donations():
+    return render_template('static/donations.html')
+
+# localhost:5050/static/contact
+@static.route('/contact')
+def static_list_contact():
+    return render_template('static/contact.html')
